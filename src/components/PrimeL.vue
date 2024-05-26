97b73ee33d86ea645188d93da7365a371104d0dc
qwerty1707 <belek5665@mail.ru>
2024-05-26 14:46:39 +0300

    чистый prime

--- src/components/PrimeL.vue
+++ src/components/PrimeL.vue
@@ -1,45 +1,5 @@
 <template>
-    <div class="demo">
-        <div class="code-playground">
-            <div class="editor-container">
-              
-                <code-editor v-model="htmlCode"></code-editor>
-            </div>
-            <div class="result-container">
-                <result-view :htmlContent="htmlCode"></result-view>
-            </div>
-        </div>
-    </div>
+  <div>
+    <h1>sdf</h1>
+  </div>
 </template>
-<script>
-// import CodeEditor from './'
-// import ResultView from './'
-
-export default {
-  name: 'CodePlayground',
-  components: {
-    // CodeEditor,
-    // ResultView
-  },
-  data() {
-    return {
-      htmlCode: '<h1>Hello, World!</h1>'
-    }
-  }
-}
-</script>
-
-<style>
-.code-playground {
-  display: flex;
-  height: 100vh;
-}
-
-.editor-container,
-.result-container {
-  flex: 1;
-  padding: 20px;
-}
-</style>
-
-
